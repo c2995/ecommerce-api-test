c78574c2687bffa26ac648e37d4f97f483269d4a
zhangsan <zhangsan@example.com>
2026-09-10 20:03:53 +0800

    feat: pytest进阶-参数化用例精准打标hook、失败重试配置

--- conftest.py
+++ conftest.py
@@ -4,4 +4,13 @@ from reset_db import  reset
 @pytest.fixture(scope="session",autouse=True)
 def reset_database():
     reset()
-    yield 
+    yield
+
+
+FLAKY_NODEIDS = {"testcases/test_runner.py::TestRunner::test_case[case63]",
+    "testcases/test_runner.py::TestRunner::test_case[case188]"}
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if item.nodeid in FLAKY_NODEIDS:
+            item.add_marker(pytest.mark.flaky(reruns=2))
